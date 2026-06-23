8d24714330660b9daad434aee0afd5b41c68d149
Faith Akanbi <fiakanbi@asu.edu>
2026-06-22 20:41:04 -0600

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.py
@@ -0,0 +1,9 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        my_map = {}
+        for word in strs:
+            if "".join(sorted(word)) not in my_map:
+                my_map["".join(sorted(word))] = []
+            my_map["".join(sorted(word))].append(word)
+
+        return list(my_map.values())
