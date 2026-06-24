0d0c1d89d30a75ae1cafeeab5ce78c63d16e12d7
Faith Akanbi <fiakanbi@asu.edu>
2026-06-24 09:27:30 -0600

    Add: string-encode-and-decode - submission-26

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-26.py
@@ -0,0 +1,33 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        #return "!@#$%".join(strs) + "!@#$%"
+        big = ""
+        for s in strs:
+            big += s + "`"
+
+        if (strs.__len__() == 0):
+            return ""
+        
+        return big
+
+    def decode(self, s: str) -> List[str]:
+        #return s.split("!@#$%")[0:-1]
+        if (s == ""):
+            return []
+        # if (s == "`"):
+        #     return [""]
+        strList = []
+        curString = ""
+        for char in s:
+            if char == "`":
+                strList.append(curString)
+                curString = ""
+            else:
+                curString += char
+        if curString.__len__() != 0:
+            strList.append(curString)
+        return strList
+
+
+
