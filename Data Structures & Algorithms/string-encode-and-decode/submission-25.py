d060abde6814495e14dda9bd6a42b4258c58ff13
Faith Akanbi <fiakanbi@asu.edu>
2026-06-24 09:26:45 -0600

    Add: string-encode-and-decode - submission-25

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-25.py
@@ -0,0 +1,33 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        
+        big = ""
+        for s in strs:
+            big += s + "`"
+
+        if (strs.__len__() == 0):
+            return ""
+        
+        return big
+
+    def decode(self, s: str) -> List[str]:
+        
+        if (s == ""):
+            return []
+        
+        strList = []
+        curString = ""
+        for char in s:
+            if char == "`":
+                strList.append(curString)
+                curString = ""
+            else:
+                curString += char
+        if curString.__len__() != 0:
+            strList.append(curString)
+            # what the??
+        return strList
+
+
+
