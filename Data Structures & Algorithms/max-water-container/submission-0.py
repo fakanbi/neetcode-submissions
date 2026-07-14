78526e60ffb93d5ed499715f2a49653d5e7489e0
Faith Akanbi <fiakanbi@asu.edu>
2026-07-14 15:05:33 -0700

    Add: max-water-container - submission-0

---
+++ Data Structures & Algorithms/max-water-container/submission-0.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        # double pointer question
+        l, r = 0, len(heights) - 1
+        maxWater = 0
+
+        while l < r:
+            #higher value first
+            distance = r - l
+            temp = min(heights[l], heights[r]) * distance
+            maxWater = max(maxWater, temp)
+            if heights[l] <= heights[r]:
+                l += 1
+            else:
+                r -= 1
+        return maxWater
+        
